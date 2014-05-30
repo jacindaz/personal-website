6bc4cd7b17cfad33c53e41fd9f9f34fddf263a34
Jacinda Zhong <jacindaz@gmail.com>
2014-05-30 17:47:20 -0400

    Adds image saturation class, applies to table
     - uses image_saturation class to make table background
       image less saturated
     - makes text more readable

--- server.rb
+++ server.rb
@@ -86,6 +86,7 @@ get '/dashboard' do
   @city = "Cambridge"
   @current_weather_object = Weather.new
 
+  #Current Weather Variables-----------------------------------------------------------------------
   @weather_info = @current_weather_object.get_current_weather(@city, "MA")
   @temperature = @current_weather_object.convert_F(@weather_info["main"]["temp"]).to_i
   @temp_min = @current_weather_object.convert_F(@weather_info["main"]["temp_min"]).to_i
@@ -94,11 +95,9 @@ get '/dashboard' do
   @weather_icon_id = @weather_info["weather"][0]["icon"]
   @weather_icon_url = @current_weather_object.weather_icon(@weather_icon_id)
 
-  erb :dashboard
-end
-
-get '/test' do
+  #Weather Forecast Variables-----------------------------------------------------------------------
   @date = today_date
+  @weather_forecast_object = Weather.new
 #!/usr/bin/ruby -w
   uri = URI("http://api.openweathermap.org/data/2.5/forecast/daily?q=Cambridge,MA&mode=xml&units=metric&cnt=7")
   response = Net::HTTP.get(uri)
@@ -107,7 +106,7 @@ get '/test' do
 
   @data = @xml_doc.xpath('/weatherdata/forecast/time[@day="2014-05-30"]')
   @temp_hash = xml_loop('/weatherdata/forecast/time[@day="2014-05-30"]/temperature', @xml_doc)
-  @temp_faren = celcius_to_faren(@temp_hash)
+  @temp_faren = @weather_forecast_object.celcius_to_faren(@temp_hash)
 
   @temp_array = []
   @xml_doc.xpath('/weatherdata/forecast/time/temperature').each do |element|
@@ -115,11 +114,18 @@ get '/test' do
     @temp_hash2 = {}
     element.each do |key,value|
       puts "each loop: key/value is #{key}, #{value}"
-      @temp_hash2[key.to_sym] = celcius_to_faren_num(value.to_f)
+      @temp_hash2[key.to_sym] = @weather_forecast_object.celcius_to_faren_num(value.to_f)
 
     end
     @temp_array << @temp_hash2
   end
+
+
+  erb :dashboard
+end
+
+get '/test' do
+
 
   #binding.pry
 
