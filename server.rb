2a9475b41b31d214054c0f640f0ba037fc25109a
Jacinda Zhong <jacindaz@gmail.com>
2014-05-29 21:33:22 -0400

    Adds Open Weather Map API information, doesn't require an API key
     - parses JSON file to get API info from Open Weather Map
     - curiously does not require an API key for this
     - returns a hash for the specified city/state

--- server.rb
+++ server.rb
@@ -2,12 +2,18 @@ require 'sinatra'
 require 'rubygems'
 require 'csv'
 require 'pry'
+require 'json'
+require 'net/http'
 
 
 
 #METHODS------------------------------------------------------------------------------------------
-
-
+def get_weather(city,state)
+  uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city},#{state}")
+  response = Net::HTTP.get(uri)
+  weather_data = JSON.parse(response)
+  return weather_data
+end
 
 
 #ROUTES AND VIEWS----------------------------------------------------------------------------------
@@ -41,6 +47,13 @@ end
 get '/blog' do
   @title = "My Blog"
   erb :blog
+end
+
+get '/dashboard' do
+  @title = "Jacinda's Dashboard"
+  @weather_info = get_weather("Cambridge", "MA")
+
+  erb :dashboard
 end
 
 #IF GET A 404 NOT FOUND ERROR--------------------------------------------
